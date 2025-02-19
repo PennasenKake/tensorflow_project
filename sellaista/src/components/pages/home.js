2421b04458c92726a24900af29ba1050c90f8942
PennasenKake <121464958+PennasenKake@users.noreply.github.com>
2025-02-19 22:45:26 +0200

    12

--- sellaista/src/components/pages/home.js
+++ sellaista/src/components/pages/home.js
@@ -1,12 +1,25 @@
 
 import React from'react';
 
+import { useState, useEffect } from'react';
+
 
 export default function Home(){
+
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:5000/api/test")
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message))
+            .catch((error) => console.error("Virhe haettaessa API:sta", error));
+    }, []);
 
     return (
         <div>
             <h1>Welcome to the Homepage</h1>
+            <p>API Viesti: {message}</p>
+
         </div>
     )
 }
